2d7319710a67056d8b70c44e1b6c5d9dcd252dc4
Eugene Vinokurov <eugene.vinokuroff@gmail.com>
2026-09-22 19:06:45 +0300

    feat: new default font (Iosevka) and removed some orphan items from start.sh script

--- start.sh
+++ start.sh
@@ -11,10 +11,10 @@ sudo pacman -S --needed git less base-devel
 
 echo "Installing core components..."
 sudo pacman -S --needed firefox kitty ghostty dolphin \
-	hyprland hyprpolkitagent hyprpaper dunst rofi xdg-desktop-portal-hyprland waybar wl-clipboard \
+	hyprland hyprpolkitagent hyprpaper quickshell rofi xdg-desktop-portal-hyprland wl-clipboard \
 	pipewire wireplumber pipewire-pulse pavucontrol \
 	grim slurp \
-	ttf-jetbrains-mono ttf-jetbrains-mono-nerd otf-font-awesome noto-fonts-cjk
+	ttf-iosevka-nerd otf-font-awesome noto-fonts-cjk
 
 echo "Configuring audio services..."
 systemctl --user enable --now pipewire.service pipewire-pulse.service wireplumber.service
@@ -24,6 +24,5 @@ git clone https://github.com/eugevin/hyprland-dotfiles.git ~/.dotfiles
 
 echo "Linking configurations..."
 rm -rf ~/.config/hypr && ln -sf ~/.dotfiles/hypr ~/.config/hypr
-rm -rf ~/.config/waybar && ln -sf ~/.dotfiles/waybar ~/.config/waybar
 rm -rf ~/.config/rofi && ln -sf ~/.dotfiles/rofi ~/.config/rofi
-#rm -rf ~/.config/dunst && ln -sf ~/.dotfiles/dunst ~/.config/dunst
+rm -rf ~/.config/quickshell && ln -sf ~/.dotfiles/quickshell ~/.config/quickshell
